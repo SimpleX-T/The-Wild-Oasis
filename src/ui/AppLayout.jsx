edcf22171f93dcb5039a87c1de449b0caae6407d
Xcodes <ntmark2004@gmail.com>
2024-05-21 13:43:31 +0100

    setting up react-hook-form and create cabin function

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,6 +13,18 @@ const StyledAppLayout = styled.div`
 const Main = styled.main`
 	padding: 4rem 4.8rem 6.4rem;
 	background-color: var(--color-grey-50);
+	overflow: scroll;
+	scrollbar-width: thin;
+	scrollbar-color: var(--color-grey-500) var(--color-grey-50);
+`;
+
+const Container = styled.div`
+	max-width: 110rem;
+	margin: 0 auto;
+
+	display: flex;
+	flex-direction: column;
+	gap: 3.3rem;
 `;
 
 function AppLayout() {
@@ -21,7 +33,9 @@ function AppLayout() {
 			<Header />
 			<Sidebar />
 			<Main>
-				<Outlet />
+				<Container>
+					<Outlet />
+				</Container>
 			</Main>
 		</StyledAppLayout>
 	);
