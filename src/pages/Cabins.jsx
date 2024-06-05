7255605504c7036f6e5c4e69d893736298b3b39b
Xcodes <ntmark2004@gmail.com>
2024-06-05 21:28:25 +0100

    adding form modal

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
+
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-	const [showForm, setShowForm] = useState(false);
 	return (
 		<>
 			<Row type='horizontal'>
@@ -15,10 +15,7 @@ function Cabins() {
 			</Row>
 			<Row>
 				<CabinTable />
-				<Button onClick={() => setShowForm((show) => !show)}>
-					Create new Cabin
-				</Button>
-				{showForm && <CreateCabinForm />}
+				<AddCabin />
 			</Row>
 		</>
 	);
