fa1b147d4ddadad708d79028a9813ea8303994fd
simplex-t <ntmark2004@gmail.com>
2024-08-28 14:39:44 +0100

    completed the booking menu, continuing with the checkin

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AppLayout from "./ui/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Account from "./pages/Account";
 import Bookings from "./pages/Bookings";
+import Booking from "./pages/Booking";
 import Cabins from "./pages/Cabins";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
@@ -33,17 +34,50 @@ function App() {
 					<Route element={<AppLayout />}>
 						<Route
 							index
-							element={<Navigate replace to='dashboard' />}
+							element={
+								<Navigate
+									replace
+									to='dashboard'
+								/>
+							}
 						/>
-						<Route path='dashboard' element={<Dashboard />} />
-						<Route path='accounts' element={<Account />} />
-						<Route path='bookings' element={<Bookings />} />
-						<Route path='cabins' element={<Cabins />} />
-						<Route path='settings' element={<Settings />} />
-						<Route path='users' element={<Users />} />
+						<Route
+							path='dashboard'
+							element={<Dashboard />}
+						/>
+						<Route
+							path='accounts'
+							element={<Account />}
+						/>
+						<Route
+							path='bookings'
+							element={<Bookings />}
+						/>
+						<Route
+							path='bookings/:bookingId'
+							element={<Booking />}
+						/>
+						<Route
+							path='cabins'
+							element={<Cabins />}
+						/>
+						<Route
+							path='settings'
+							element={<Settings />}
+						/>
+						<Route
+							path='users'
+							element={<Users />}
+						/>
 					</Route>
-					<Route path='login' element={<Login />} />
-					<Route path='*' element={<PageNotFound />} />
+					<Route
+						path='login'
+						element={<Login />}
+					/>
+					<Route
+						path='*'
+						element={<PageNotFound />}
+					/>
 				</Routes>
 			</BrowserRouter>
 			<Toaster
--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -9,44 +9,54 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "./useBooking";
+import Spinner from "../../ui/Spinner";
 
 const HeadingGroup = styled.div`
-  display: flex;
-  gap: 2.4rem;
-  align-items: center;
+	display: flex;
+	gap: 2.4rem;
+	align-items: center;
 `;
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
+	const { booking, isLoading } = useBooking();
 
-  const moveBack = useMoveBack();
+	// console.log(booking);
 
-  const statusToTagName = {
-    unconfirmed: "blue",
-    "checked-in": "green",
-    "checked-out": "silver",
-  };
+	// const booking = {};
+	const status = booking.status || "";
 
-  return (
-    <>
-      <Row type="horizontal">
-        <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
-        </HeadingGroup>
-        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
-      </Row>
+	const moveBack = useMoveBack();
 
-      <BookingDataBox booking={booking} />
+	const statusToTagName = {
+		unconfirmed: "blue",
+		"checked-in": "green",
+		"checked-out": "silver",
+	};
 
-      <ButtonGroup>
-        <Button variation="secondary" onClick={moveBack}>
-          Back
-        </Button>
-      </ButtonGroup>
-    </>
-  );
+	return (
+		<>
+			<Row type='horizontal'>
+				<HeadingGroup>
+					<Heading as='h1'>Booking #{booking.id}</Heading>
+					<Tag type={statusToTagName[status]}>
+						{status.replace("-", " ")}
+					</Tag>
+				</HeadingGroup>
+				<ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+			</Row>
+			{isLoading && <Spinner />}
+			{booking.guests && <BookingDataBox booking={booking} />}
+
+			<ButtonGroup>
+				<Button
+					$variation='secondary'
+					onClick={moveBack}>
+					Back
+				</Button>
+			</ButtonGroup>
+		</>
+	);
 }
 
 export default BookingDetail;
